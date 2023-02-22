3e1fda012afee0adb97d930c2b3d05dbc76abda8
Elalia Nghiikumbu <hnghiikumbu@gmail.com>
2023-02-22 05:40:38 -0800

    shell

--- 0-betty_checks.c
+++ 0-betty_checks.c
@@ -1,25 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-/**
- * main - Prints a random number and states whether
- *
- *        it is positive, negative, or zero.
- *
- * Return: Always 0.
- */
-int main(void)
+int main()
 {
-	int n;
+	char *command = NULL;
+	size_t len = 0;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
-	return (0);
+	char *argv[] = {"/bin/ls", NULL};
+
+	while (1)
+	{
+		printf("$");
+		getline(&command, &len, stdin);
+		printf("%s", command);
+
+		execve(argv[0], argv, NULL);
+	}
+	return 0;
 }
