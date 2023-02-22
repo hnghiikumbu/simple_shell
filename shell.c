59f3a67e90302e094200adf6f446b4804faa6cbb
Elalia Nghiikumbu <hnghiikumbu@gmail.com>
2023-02-22 11:25:46 -0800

    shell

--- shell.c
+++ shell.c
@@ -16,10 +16,13 @@ int main(void)
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/** print positive if n greater than 0 */
 	if (n > 0)
 		printf("%d is positive\n", n);
+	/** print negative if n lesst than 0 */
 	else if (n < 0)
 		printf("%d is negative\n", n);
+	/** print zero if n is equal to 0 */
 	else
 		printf("%d is zero\n", n);
 	return (0);
